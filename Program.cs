d390d1eee66696d270ca717f1657a1ba14d2e770
Drakula <vbuzinskij@gmail.com>
2023-03-14 02:39:22 +0300

    DZ8 Sem

--- Program.cs
+++ Program.cs
@@ -22,21 +22,6 @@
     }
     return result;
 }
-// Int32[] result = InputCheck();
-
-void Outputting(double[,] InputArray)
-{
-    for (int i = 0; i < InputArray.GetLength(0); i++)
-    {
-
-        for (int j = 0; j < InputArray.GetLength(1); j++)
-        {
-
-            Console.Write(InputArray[i, j] + " ");
-        }
-        Console.WriteLine("");
-    }
-}
 
 void OutputtingInt(Int32[,] InputArray)
 {
@@ -52,73 +37,141 @@ void OutputtingInt(Int32[,] InputArray)
     }
 }
 
-
-// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
-Console.WriteLine("Задача 47. Введите размер массива, максимальное и минимальное значение элемента массива через запятую'm,n,max,min'");
-Int32[] Input = InputCheck();
-if (Input[0] == 0 && Input.Length == 5)
+Int32[,] BuildArray(Int32 y, Int32 x)
 {
-    Double[,] array = new Double[Input[1], Input[2]];
-    Int32 max = Input[3];
-    Int32 min = Input[4];
-    for (int i = 0; i < Input[1]; i++)
-    {
+    Int32[,] Array = new Int32[y, x];
 
-        for (int j = 0; j < Input[2]; j++)
+    for (int i = 0; i < y; i++)
+    {
+        for (int j = 0; j < x; j++)
         {
-            array[i, j] = (new Random().NextDouble()) * (max - min) + min;
+            Array[i, j] = new Random().Next(0, 20);
         }
     }
-    Outputting(array);
+    return Array;
 }
-else
+Int32[] SortArray(Int32[] array)
 {
-    Console.WriteLine("Ошибка");
-}
-
-// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента 
-// или же указание, что такого элемента нет.
-Console.WriteLine("Задача 50. Введите позицию элемента в массиве'строка,столбец'");
-Int32[] Input50 = InputCheck();
-if (Input50[0] == 0 && Input50.Length == 3)
-{
-    Int32[,] array = new Int32[new Random().Next(1, 10), new Random().Next(1, 10)];
-    for (int i = 0; i < array.GetLength(0); i++)
+    Int32 TimeVariable = 0;
+    Int32 j = 0;
+    for (int i = 1; i < array.Length; i++)
     {
-        for (int j = 0; j < array.GetLength(1); j++)
+        j = i;
+        while ((j > 0) && (array[j] > array[j - 1]))
         {
-            array[i, j] = new Random().Next(0, 30);
+            TimeVariable = array[j - 1];
+            array[j - 1] = array[j];
+            array[j] = TimeVariable;
+            j = j - 1;
         }
     }
-    OutputtingInt(array);
-    if (Input50[1] < array.GetLength(0) && Input50[2] < array.GetLength(1))
-    {
-        Console.WriteLine(array[Input50[1] - 1, Input50[2] - 1]);
-    }
-    else
-    {
-        Console.WriteLine("такого числа в массиве нет");
-    }
-
-    //     Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
-
-
-    Console.WriteLine("Задача 52.");
-    OutputtingInt(array);
-
-    double average;
+    return array;
+}
+Int32[] CutArray(Int32[,] array, Int32 Line)
+{
+    Int32[] Result = new Int32[array.GetLength(1)];
     for (int i = 0; i < array.GetLength(1); i++)
     {
-        Int32 sum = 0;
-        for (int j = 0; j < array.GetLength(0); j++)
-        {
-            sum = sum + array[j, i];
-        }
-        average = Convert.ToDouble(sum) / array.GetLength(0);
-        Console.Write(average + "  ");
+        Result[i] = array[Line, i];
     }
+    return Result;
+}
+
+
+
+// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Console.WriteLine("Задача 54. Введите размер массива (Столбцы, Строки)");
+Int32[] Input = InputCheck();
+if (Input[0] == 0 && Input.Length == 3)
+{
+    Int32[,] ResultArray = new Int32[Input[1], Input[2]];
+    Int32[,] Array = BuildArray(Input[1], Input[2]);
+    Int32[] TimeArray = new Int32[Array.GetLength(1)];
+
+    OutputtingInt(Array);
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        TimeArray = CutArray(Array, i);
+        TimeArray = SortArray(TimeArray);
+        for (int j = 0; j < Array.GetLength(1); j++)
+        {
+            ResultArray[i, j] = TimeArray[j];
+        }
+    }
+    OutputtingInt(ResultArray);
 }
 else
 {
     Console.WriteLine("Ошибка");
 }
+
+//Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+Console.WriteLine("Задача 56. Введите размер массива (Столбцы, Строки)");
+Int32[] Input56 = InputCheck();
+if (Input56[0] == 0 && Input56.Length == 3)
+{
+    Int32 PrevSum = 0;
+    Int32 Result = 0;
+    Int32[,] Array = BuildArray(Input56[1], Input56[2]);
+    Int32[] TimeArray = new Int32[Array.GetLength(1)];
+    OutputtingInt(Array);
+    Int32 Sum = 0;
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        Sum = 0;
+        TimeArray = CutArray(Array, i);
+        for (int j = 0; j < TimeArray.Length; j++)
+        {
+            Sum = Sum + TimeArray[j];
+        }
+        if (Sum < PrevSum)
+        {
+            Result = i;
+        }
+        PrevSum = Sum;
+    }
+    Console.WriteLine(Convert.ToString(Result));
+}
+else
+{
+    Console.WriteLine("Ошибка");
+}
+
+
+
+//Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+
+Console.WriteLine("Задача 56. Введите размер массива (Столбцы Матрицы А, Строки Матрицы А, толбцы Матрицы Б, Строки Матрицы Б)");
+Int32[] Input58 = InputCheck();
+if (Input58[0] == 0 && Input58.Length == 5 && Input58[1] == Input58[4])
+{
+    Int32[,] ArrayA = BuildArray(Input58[1], Input58[2]);
+    Int32[,] ArrayB = BuildArray(Input58[3], Input58[4]);
+    Int32[,] Result = new Int32[Input58[1], Input58[4]];
+    OutputtingInt(ArrayA);
+    OutputtingInt(ArrayB);
+    for (int i = 0; i < ArrayA.GetLength(0); i++)
+    {
+        for (int j = 0; j < ArrayB.GetLength(1); j++)
+        {
+            for (int k = 0; k < ArrayB.GetLength(0); k++)
+            {
+                Result[i, j] += ArrayA[i, k] * ArrayB[k, j];
+            }
+        }
+    }
+
+    OutputtingInt(Result);
+
+}
+else
+{
+    Console.WriteLine("Ошибка");
+}
+
+
+
+
+
+
+
